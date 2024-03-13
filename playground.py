d64a373a4695b84034b37858daafc207d711296a
Gabriel Sánchez-Blanco <gabriel.sblancor@gmail.com>
2024-03-13 18:03:05 +0100

    function transform data

---
+++ playground.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import pandas as pd
+
+x = np.array([[1],[2],[3],[4],[5]])
+
+print("Vector x: \n",x)
+
+sin_x = np.sin(x)
+
+print("Vector sin_x: \n",sin_x)
+
+exp_x = np.exp(x)
+
+print("Vector exp_x: \n",exp_x)
