46eb8f26db8c00b545152f73aae7028576763fb4
Gabriel-SBR <83508724+Gabriel-SBR@users.noreply.github.com>
2024-03-20 12:51:09 +0100

    Renamed Folders and implemented functions

--- playground-gabriel.py
+++ playground-gabriel.py
@@ -1,3 +1,4 @@
+"""
 import numpy as np 
 import pandas as pd
 
@@ -14,3 +15,29 @@ exp_x = np.exp(x)
 print("Vector exp_x: \n",exp_x)
 
 x_squared = np.square(x)
+
+print(x.size)
+
+RMSE = np.sqrt(np.mean(np.square(x)))
+
+print("RMSE: ", RMSE)
+
+for i in range(10):
+    print(i)
+
+import numpy as np
+from sklearn.model_selection import KFold
+X = np.array([[1, 2], [3, 4], [1, 2], [3, 4]])
+y = np.array([1, 2, 3, 4])
+kf = KFold(n_splits=4,shuffle=True)
+kf.get_n_splits(X)
+print(kf)
+
+
+for i, (train_index, test_index) in enumerate(kf.split(X)):
+    print(f"Fold {i}:")
+    print(f"  Train: index={train_index}")
+    print(f"  Test:  index={test_index}")
+
+"""
+
